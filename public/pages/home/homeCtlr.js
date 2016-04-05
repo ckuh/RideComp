1629e02acafe5a8fcec5ac6e336fe998c9d12962
Chris Kuh <kuh_chris@yahoo.com>
2016-04-05 04:18:46 -0700

    [feature] auto set curLoc

--- public/pages/home/homeCtlr.js
+++ public/pages/home/homeCtlr.js
@@ -5,58 +5,50 @@ angular.module('App')
     vm.spinner = false;
     $localStorage.user = {};
 
-    vm.saveUserLoc = function(){
-      if(vm.user.curLoc.geometry){
+    vm.saveUserLoc = function() {
+      if (vm.user.curLoc.geometry) {
         $localStorage.user.curLat = vm.user.curLoc.geometry.location.lat();
         $localStorage.user.curLng = vm.user.curLoc.geometry.location.lng();
-      }else {
+      } else {
         $localStorage.user.curLat = vm.autoLat;
         $localStorage.user.curLng = vm.autoLng;
       }
 
-      if(vm.user.endLoc.geometry){
+      if (vm.user.endLoc.geometry) {
         $localStorage.user.endLat = vm.user.endLoc.geometry.location.lat();
         $localStorage.user.endLng = vm.user.endLoc.geometry.location.lng();
-      }else {
+      } else {
         $localStorage.user.endLat = vm.autoLat;
         $localStorage.user.endLng = vm.autoLng;
       }
       $state.go('comp');
     }
 
-    vm.swap = function(){
+    vm.swap = function() {
       var hold = vm.user.curLoc;
       vm.user.curLoc = vm.user.endLoc;
       vm.user.endLoc = hold;
     }
 
-    vm.getCurLoc = function(){
+    vm.getCurLoc = function() {
       vm.spinner = true;
       vm.user.curLoc = {};
       geolocation.getLocation()
-      .then(function(data){
-        vm.autoLat = data.coords.latitude;
-        vm.autoLng = data.coords.longitude;
+        .then(function(data) {
+          vm.autoLat = data.coords.latitude;
+          vm.autoLng = data.coords.longitude;
 
-        homeFactory.getCurLocString(data.coords.latitude,data.coords.longitude)
-        .then(function(data){
-          vm.spinner = false;
-          vm.user.curLoc = data[0].formatted_address;
-        })
-      });
-      // if(navigator.geolocation){
-      //   navigator.geolocation.getCurrentPosition(function(position){
-      //     debugger;
-      //     homeFactory.getCurLocString(data.coords.latitude,data.coords.longitude)
-      //     .then(function(data){
-      //       vm.spinner = false;
-      //       vm.user.curLoc = data[0].formatted_address;
-      //     })
-      //   })
-      // }
+          homeFactory.getCurLocString(data.coords.latitude, data.coords.longitude)
+            .then(function(data) {
+              vm.spinner = false;
+              vm.user.curLoc = data[0].formatted_address;
+            })
+        });
     }
 
-    vm.reset = function(){
+    vm.reset = function() {
       vm.user = {};
     }
+
+    vm.getCurLoc();
   });
